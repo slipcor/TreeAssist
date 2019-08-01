8a04a042b609ac22688bd9c2b84cc3b45a3d0515
pop4959 <pop4959@gmail.com>
2019-08-01 10:51:08 -0700

    Configurable lookup time for CoreProtect blocklist (#32)

--- src/me/itsatacoshop247/TreeAssist/blocklists/CoreProtectBlockList.java
+++ src/me/itsatacoshop247/TreeAssist/blocklists/CoreProtectBlockList.java
@@ -13,10 +13,11 @@ import java.util.List;
 
 public class CoreProtectBlockList implements BlockList {
 	private final CoreProtectAPI protect;
-	private static final int LOOKUP_TIME = 60*60*24;
+	private int lookupTime;
 	
 	public CoreProtectBlockList() {
 		protect = getCoreProtect();
+		lookupTime = Utils.plugin.getConfig().getInt("Placed Blocks.Handler Lookup Time", 60*60*24);
 	}
 	private CoreProtectAPI getCoreProtect() {
 		Plugin plugin = Bukkit.getPluginManager().getPlugin("CoreProtect");
@@ -43,10 +44,10 @@ public class CoreProtectBlockList implements BlockList {
 
 	@Override
 	public boolean isPlayerPlaced(Block block) {
-		if (protect == null) {
+		if (protect == null || lookupTime <= 0) {
 			return false;
 		}
-		List<String[]> lookup = protect.blockLookup(block, LOOKUP_TIME);
+		List<String[]> lookup = protect.blockLookup(block, lookupTime);
 
 		for (String[] value : lookup) {
 			ParseResult result = protect.parseResult(value);
