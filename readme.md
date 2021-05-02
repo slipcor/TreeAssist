27d6c02982cccccb201cf459b1f89890afc1202f
Chris <chris@slipcor.de>
2021-05-03 01:51:48 +0200

    v7.2.3 - prevent players from dropping a sapling if that sapling would and should not grow

--- readme.md
+++ readme.md
@@ -73,7 +73,7 @@ It also will take down an entire tree when it is enabled in the config.
 
 ## Changelog
 
-- v7.2.2 - fix shading package - no functional differences to 7.2.1!
+- v7.2.3 - prevent players from dropping a sapling if that sapling would and should not grow
 - [read more](doc/changelog.md)
 
 ***
--- src/main/java/net/slipcor/treeassist/listeners/TreeAssistSpawnListener.java
+++ src/main/java/net/slipcor/treeassist/listeners/TreeAssistSpawnListener.java
@@ -6,12 +6,20 @@ import net.slipcor.treeassist.events.TASaplingPlaceEvent;
 import net.slipcor.treeassist.runnables.TreeAssistSaplingSelfPlant;
 import net.slipcor.treeassist.utils.MaterialUtils;
 import net.slipcor.treeassist.yml.TreeConfig;
+import org.bukkit.Bukkit;
+import org.bukkit.Material;
+import org.bukkit.block.Block;
+import org.bukkit.block.BlockFace;
 import org.bukkit.entity.Item;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
+import org.bukkit.event.block.Action;
 import org.bukkit.event.entity.EntityPickupItemEvent;
 import org.bukkit.event.entity.ItemSpawnEvent;
+import org.bukkit.event.player.PlayerDropItemEvent;
+import org.bukkit.event.player.PlayerInteractEvent;
+import org.bukkit.inventory.ItemStack;
 
 import java.util.Random;
 
@@ -24,6 +32,35 @@ public class TreeAssistSpawnListener implements Listener {
 	{
 		plugin = instance;
 	}
+
+	@EventHandler(priority = EventPriority.HIGHEST, ignoreCancelled = true)
+	public void itemDropEvent(PlayerDropItemEvent event) {
+		Item drop = event.getItemDrop();
+		for (String name : TreeAssist.treeConfigs.keySet()) {
+			TreeConfig config = TreeAssist.treeConfigs.get(name);
+			if (config.getMaterial(TreeConfig.CFG.REPLANTING_MATERIAL) == drop.getItemStack().getType()) {
+				debug.i("[PlayerDrop] Someone dropped our sapling! - " + name);
+				if (!config.getBoolean(TreeConfig.CFG.REPLANTING_DROPPED_SAPLINGS)) {
+					debug.i("[PlayerDrop] But we do not want to plant this anyway");
+					continue;
+				}
+
+				Block landingBlock = event.getPlayer().getLocation().getBlock();
+				while (landingBlock.isEmpty()) {
+					landingBlock = landingBlock.getRelative(BlockFace.DOWN);
+				}
+				PlayerInteractEvent interactEvent = new PlayerInteractEvent(event.getPlayer(), Action.RIGHT_CLICK_BLOCK, new ItemStack(Material.BONE_MEAL, 1),
+						landingBlock, BlockFace.UP);
+
+				Bukkit.getPluginManager().callEvent(interactEvent);
+
+				if (interactEvent.isCancelled()) {
+					event.setCancelled(true);
+					return;
+				}
+			}
+		}
+	}
 	
 	@EventHandler(priority = EventPriority.HIGHEST, ignoreCancelled = true)
 	public void itemSpawnEvent(ItemSpawnEvent event) {
@@ -31,10 +68,10 @@ public class TreeAssistSpawnListener implements Listener {
 		for (String name : TreeAssist.treeConfigs.keySet()) {
 			TreeConfig config = TreeAssist.treeConfigs.get(name);
 			if (config.getMaterial(TreeConfig.CFG.REPLANTING_MATERIAL) == drop.getItemStack().getType() ) {
-				debug.i("Someone dropped our sapling! - " + name);
+				debug.i("[ItemSpawn] Someone dropped our sapling! - " + name);
 				if (!config.getBoolean(TreeConfig.CFG.REPLANTING_DROPPED_SAPLINGS)) {
-					debug.i("But we do not want to plant this anyway");
-					return;
+					debug.i("[ItemSpawn] But we do not want to plant this anyway");
+					continue;
 				}
 
 				TASaplingPlaceEvent newEvent = new TASaplingPlaceEvent(
@@ -42,7 +79,7 @@ public class TreeAssistSpawnListener implements Listener {
 						drop.getItemStack().getType());
 				TreeAssist.instance.getServer().getPluginManager().callEvent(newEvent);
 				if (newEvent.isCancelled()) {
-					debug.i("TreeAssistSpawnListener.itemSpawnEvent() Sapling Replant was cancelled!");
+					debug.i("[ItemSpawn] Sapling Replant was cancelled!");
 					continue;
 				}
 
